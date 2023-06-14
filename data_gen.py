34b85c07c6cf71f887105397f58190b3b4fa6cee
Berkan Tim Ottlik <bto2106@ruth.cm.cluster>
2023-06-14 17:18:13 -0400

    training on server and starting evaluation comparing transfomer to manual model selection

--- data_gen.py
+++ data_gen.py
@@ -4,22 +4,40 @@ import torch
 def get_data(alphas=[0.1, 0.5], N=21, d_d=5, train_samp_per_class=10000):
     data = {a: {b: {"y_test": 0, "y_hat": 0} for b in alphas} for a in alphas}
 
+    train_samples_per_alpha = int(train_samp_per_class / len(alphas))
+
     for a in alphas:
-        x_train = torch.normal(0, 1, size=(train_samp_per_class, N, d_d))
-        x_test = torch.normal(0, 1, size=(train_samp_per_class, N, d_d))
+        X_train = torch.normal(0, 1, size=(train_samples_per_alpha, N, d_d))
+        X_test = torch.normal(0, 1, size=(train_samples_per_alpha, N, d_d))
 
-        w = torch.normal(0, 1, size=(train_samp_per_class, d_d, 1))
+        w = torch.normal(0, 1, size=(train_samples_per_alpha, d_d, 1))
 
-        y_train = torch.squeeze(torch.matmul(x_train, w)) + torch.normal(0, a*1.5, size=(train_samp_per_class, N))
-        y_test = torch.squeeze(torch.matmul(x_test, w)) + torch.normal(0, a*1.5, size=(train_samp_per_class, N))
+        y_train = torch.squeeze(torch.matmul(X_train, w)) + torch.normal(0, a*0.8, size=(train_samples_per_alpha, N))
+        y_test = torch.squeeze(torch.matmul(X_test, w)) + torch.normal(0, a*0.8, size=(train_samples_per_alpha, N))
 
         for b in alphas:
-            results = torch.zeros((train_samp_per_class, N))
-            for i in range(train_samp_per_class):
+            results = torch.zeros((train_samples_per_alpha, N))
+            for i in range(train_samples_per_alpha):
                 reg = linear_model.Ridge(alpha=b)
-                reg.fit(x_train[i], y_train[i])
-                results[i] = torch.from_numpy(reg.predict(x_test[i]))
+                reg.fit(X_train[i], y_train[i])
+                results[i] = torch.from_numpy(reg.predict(X_test[i]))
             data[a][b]["y_hat"] = results
             data[a][b]["y_test"] = y_test
 
     return data
+
+def format_data(data_dict, train_samples_per_alpha=10000):
+    alphas_merged = torch.cat([torch.ones(train_samples_per_alpha)*a for a in data_dict], dim=0)
+    X_merged = torch.cat([torch.cat([torch.unsqueeze(data_dict[a][b]["y_hat"], 2) for b in data_dict[a]] + [torch.unsqueeze(data_dict[a][a]["y_test"], 2)], dim=2) for a in data_dict], dim=0)
+    X_merged[:, -1, -1] = 0
+    y_merged = torch.cat([data_dict[a][a]["y_test"][:, -1] for a in data_dict], dim=0)
+
+    randperm = torch.randperm(alphas_merged.shape[0])
+
+    alphas = alphas_merged[randperm]
+    X = X_merged[randperm]
+    y = y_merged[randperm]
+
+    print(f"Alphas: {alphas.shape}, X: {X.shape}, y: {y.shape}")
+
+    return alphas, X, y
--- main.py
+++ main.py
@@ -2,18 +2,17 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader
-from data_gen import get_data
+from data_gen import get_data, format_data
 from models import TransformerModel
 import wandb
 import yaml
 from munch import Munch
+import time
 
 with open(f"configs/model_selection.yaml", "r") as yaml_file:
     args = Munch.fromYAML(yaml_file)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-data_dict = get_data(alphas=args.data.data_alphas, N=args.data.N, d_d=args.data.d_d, train_samp_per_class=args.data.train_samp_per_class)
 
 def nn_train(dataloader, model, loss_fn, optimizer, verbose=False):
     size = len(dataloader.dataset)
@@ -32,9 +31,8 @@ def nn_train(dataloader, model, loss_fn, optimizer, verbose=False):
         loss.backward()
         optimizer.step()
 
-        if batch % 5 == 0:
-            loss, current = loss.item(), batch * len(X)
         if verbose:
+            loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
     return avg_loss/size
@@ -56,20 +54,32 @@ def nn_test(dataloader, model, loss_fn, verbose=False):
 
     return test_loss
 
-def train(model, X_train, y_train, X_test, y_test):
+def train(model):
     optimizer = torch.optim.Adam(model.parameters(), lr=args.training.lr)
     loss_fn = nn.MSELoss()
-
-    train_dataset = TensorDataset(X_train, y_train)
-    test_dataset = TensorDataset(X_test, y_test)
-
-    train_dataloader = DataLoader(train_dataset, batch_size=args.training.batch_size)
-    test_dataloader = DataLoader(test_dataset, batch_size=args.training.batch_size)
 
     train_loss = []
     test_loss = []
 
     for t in range(args.training.epochs):
+        data_dict = get_data(alphas=args.data.data_alphas, N=args.data.N, d_d=args.data.d_d, train_samp_per_class=args.data.train_samp_per_class)
+
+        alphas, X, y = format_data(data_dict, train_samp_per_class=int(args.data.train_samp_per_class / len(args.data.data_alphas)))
+
+        alphas_train = alphas[:int(alphas.shape[0]*args.data.train_prop)]
+        alphas_test = alphas[int(alphas.shape[0]*args.data.train_prop):]
+
+        X_train = X[:int(X.shape[0]*args.data.train_prop)]
+        X_test = X[int(X.shape[0]*args.data.train_prop):]
+        y_train = y[:int(y.shape[0]*args.data.train_prop)]
+        y_test = y[int(y.shape[0]*args.data.train_prop):]
+
+        train_dataset = TensorDataset(X_train, y_train)
+        test_dataset = TensorDataset(X_test, y_test)
+
+        train_dataloader = DataLoader(train_dataset, batch_size=args.training.batch_size)
+        test_dataloader = DataLoader(test_dataset, batch_size=args.training.batch_size)
+
         new_train_loss = nn_train(train_dataloader, model, loss_fn, optimizer)
         new_test_loss = nn_test(test_dataloader, model, loss_fn)
         train_loss.append(new_train_loss)
@@ -80,7 +90,8 @@ def train(model, X_train, y_train, X_test, y_test):
             print(f"Train Loss: {new_train_loss}, Test Loss: {new_test_loss}")
         
         if t % args.training.save_every_epochs == 0:
-            pass
+            print(f"Saving model at epoch {t} to {args.out_dir}/model{int(time.time()*10)}.pth")
+            torch.save(model.state_dict(), f"{args.out_dir}/model{int(time.time()*10)}.pth")
         
         wandb.log(
             {
@@ -90,50 +101,26 @@ def train(model, X_train, y_train, X_test, y_test):
             step=t,
         )
     
-    torch.save(model.state_dict(), f"{args.out_dir}/model.pth")
+    torch.save(model.state_dict(), f"{args.out_dir}/model{int(time.time()*10)}.pth")
 
     return train_loss, test_loss
 
-def format_data(data_dict):
-    alphas_merged = torch.cat([torch.ones(args.data.train_samp_per_class)*a for a in data_dict], dim=0)
-    X_merged = torch.cat([torch.cat([torch.unsqueeze(data_dict[a][b]["y_hat"], 2) for b in data_dict[a]] + [torch.unsqueeze(data_dict[a][a]["y_test"], 2)], dim=2) for a in data_dict], dim=0)
-    X_merged[:, -1, -1] = 0
-    y_merged = torch.cat([data_dict[a][a]["y_test"][:, -1] for a in data_dict], dim=0)
+if __name__ == "__main__":
+    model = TransformerModel(
+        n_dims=len(args.data.data_alphas) + 1,
+        n_positions=args.data.N,
+        n_layer=args.model.n_layer,
+        n_head=args.model.n_head,
+        n_embd=args.model.n_embd
+    ).to(device)
 
-    randperm = torch.randperm(alphas_merged.shape[0])
+    wandb.init(dir=args.out_dir,
+        project=args.wandb.project,
+        entity=args.wandb.entity,
+        config=args.__dict__,
+        notes=args.wandb.notes,
+        name=args.wandb.name,
+        resume=True
+    )
 
-    alphas = alphas_merged[randperm]
-    X = X_merged[randperm]
-    y = y_merged[randperm]
-
-    print(f"Alphas: {alphas.shape}, X: {X.shape}, y: {y.shape}")
-
-    return alphas, X, y
-
-alphas, X, y = format_data(data_dict)
-
-alphas_train = alphas[:int(alphas.shape[0]*args.data.train_prop)]
-alphas_test = alphas[int(alphas.shape[0]*args.data.train_prop):]
-X_train = X[:int(X.shape[0]*args.data.train_prop)]
-X_test = X[int(X.shape[0]*args.data.train_prop):]
-y_train = y[:int(y.shape[0]*args.data.train_prop)]
-y_test = y[int(y.shape[0]*args.data.train_prop):]
-
-model = TransformerModel(
-    n_dims=len(args.data.data_alphas) + 1,
-    n_positions=args.data.N,
-    n_layer=args.model.n_layer,
-    n_head=args.model.n_head,
-    n_embd=args.model.n_embd
-)
-
-wandb.init(dir=args.out_dir,
-    project=args.wandb.project,
-    entity=args.wandb.entity,
-    config=args.__dict__,
-    notes=args.wandb.notes,
-    name=args.wandb.name,
-    resume=True
-)
-
-train_loss, test_loss = train(model, X_train, y_train, X_test, y_test)
+    train_loss, test_loss = train(model)
