e86c995de6bbc0e965cf4adc82562961af0148b0
Edward <ironedward8@gmail.com>
2023-06-23 23:52:30 -0400

    cleaned up code a bit

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # Interpretability of Small Model Selection Model
-Epic dude 😎.
-
-## Setup
-You need to create a config file called `configs/model_selection.yaml` using `configs/TEMPLATE.yaml` to run `main.ipynb`.
---
+++ src/train.py
@@ -0,0 +1,32 @@
+import torch
+from utils.models import TransformerModel
+from utils.training import *
+import wandb
+from munch import Munch
+import argparse
+
+
+# Command line parser
+parser = argparse.ArgumentParser()
+parser.add_argument("--config_file", type=str)
+cmd_args = vars(parser.parse_args())
+
+args = Munch.fromYAML(open(cmd_args.config_file, "r"))
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = TransformerModel(n_dims=len(args.data.data_alphas) + 1, 
+                         n_positions=args.data.N,
+                         n_layer=args.model.n_layer,
+                         n_head=args.model.n_head,
+                         n_embd=args.model.n_embd)
+model.to(device)
+
+wandb.init(dir=args.out_dir,
+           project=args.wandb.project,
+           entity=args.wandb.entity,
+           config=args.__dict__,
+           notes=args.wandb.notes,
+           name=args.wandb.name,
+           resume=False)
+
+train_loss, test_loss = train(model, device, args)
